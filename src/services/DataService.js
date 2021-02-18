cf0c4fe37fa31ebe4a5fa0382b2cdfa8094e3518
slavek <jaroslav.psenicka@gmail.com>
2021-02-18 22:00:35 +0100

    lepší obsluha serverových chyb

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 const SERVICE_URL = process.env.REACT_APP_SERVICE_URL || 'http://localhost:8080'
+const NO_UPLOADID = { message: 'No upload ID given by the server' }
+const REFUSED = { message: 'Refused by the server' }
 
 export const readData = () => {
   return new Promise((resolve, reject) => {
@@ -16,19 +18,20 @@ export const uploadNameAndHeight = (name, height) => {
   }
   return new Promise((resolve, reject) => {
     axios.post(`${SERVICE_URL}/submit`, { name, height }, config)
-      .then(response => resolve(response.data.uploadId))
+      .then(response => response.data.uploadId ? resolve(response.data.uploadId) : reject(NO_UPLOADID))
       .catch(err => reject(err))
   })
 }
 
 export const uploadFiles = (uploadId, files) => {
   return new Promise((resolve, reject) => {
+    if (!files) resolve()
     if (files.length === 0) resolve()
 
     const formData = new FormData()
     formData.append('file', files[0])
     axios.post(`${SERVICE_URL}/upload/${uploadId}`, formData)
-      .then(response => resolve())
+      .then(response => response.data.result ? resolve() : reject(REFUSED))
       .catch(err => reject(err))
   })
 }
